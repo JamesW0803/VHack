04477f6fa499a7da26a6178ebd9f116f6df4b960
James Wong <jwyn0803@gmail.com>
2024-03-10 15:32:02 +0800

    Added homepage

---
+++ mobile.js
@@ -0,0 +1,23 @@
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+window.addEventListener('scroll', function(){
+  var header = document.getElementById('mainHeader');
+  var scrollPosition = window.pageYOffset || this.document.documentElement.scrollTop;
+
+  if (scrollPosition > 100) { 
+    header.style.backgroundColor = 'rgb(211,211,211,0.5)'; 
+  } else {
+    header.style.backgroundColor = 'rgba(211, 211, 211, 1)'; 
+  }
+})
